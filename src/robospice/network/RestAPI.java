30ee3250344f692f6f1799a36a0e531ddca98938
amit.yadav <amit.yadav@kore.com>
2015-03-05 14:52:16 +0530

    RestAPI return type is not coupled with other parts

--- src/robospice/network/RestAPI.java
+++ src/robospice/network/RestAPI.java
@@ -8,6 +8,11 @@ public interface RestAPI {
 	
 	/**
 	 * Notice the Response this should be match throughout the Project
+	 * 
+	 * Not necessary this might be different with loadDataFromNetwork()
+	 * 
+	 * say, here we have ArrayList<String> and in loadDataFromNetwork() we can simply return String like below
+	 * ArrayList<String>.get(0)
 	 */
 	@GET("/data/2.5/weather")
 	Response getWeather(@Query("q") String city);	
